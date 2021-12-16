8c796169573bd449cc9b356efc28691f206d82f0
Frédéric Demarle <frederic.demarle@yahoo.fr>
2021-12-16 18:44:16 +0100

    Add PHPunit test for full load

--- tests/phpunit/tests/test-specific.php
+++ tests/phpunit/tests/test-specific.php
@@ -11,8 +11,31 @@ class Specific_Test extends WP_UnitTestCase {
 		unset( $GLOBALS['l10n'] );
 	}
 
-	public function test_shipment_tracking() {
+	public function mo_provider() {
+		return array(
+			array( 'WP_Syntex\DynaMo\Dynamic\MO' ),
+			array( 'WP_Syntex\DynaMo\Full\MO' ),
+		);
+	}
+
+	protected function init( $class ) {
+		add_filter(
+			'dynamo_file_loader',
+			function() use ( $class ) {
+				return new $class();
+			}
+		);
 		( new Plugin() )->add_hooks();
+	}
+
+	/**
+	 * Test a WC Shipment tracking translation file which generated a notice
+	 * during the development of the inital version.
+	 *
+	 * @dataProvider mo_provider
+	 */
+	public function test_shipment_tracking( $class ) {
+		$this->init( $class );
 		load_textdomain( 'shipment-tracking', TEST_DATA_DIR . 'woocommerce-shipment-tracking-fr_FR.mo' );
 
 		// Call done by _get_plugin_data_markup_translate().
--- tests/phpunit/tests/test-translations-binary-search.php
+++ tests/phpunit/tests/test-translations-binary-search.php
@@ -8,6 +8,12 @@ class Translations_Binary_Search_Test extends WP_UnitTestCase {
 	use Translations_Test_Trait;
 
 	public function set_up() {
+		add_filter(
+			'dynamo_file_loader',
+			function() {
+				return new \WP_Syntex\DynaMo\Dynamic\MO();
+			}
+		);
 		( new Plugin() )->add_hooks();
 		load_textdomain( 'default', TEST_DATA_DIR . 'sl_SI_without_hash_table.mo' );
 	}
