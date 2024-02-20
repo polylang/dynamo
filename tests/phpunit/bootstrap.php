eac1b1e1380ca55567ab22478995cc21cbfae4ed
Hug0-Drelon <hugo.drelon@gmail.com>
2024-02-20 16:59:35 +0100

    Bad assignation.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -2,7 +2,6 @@
 
 $_root_dir  = dirname( __DIR__, 2 );
 $_tests_dir = ! empty( getenv( 'WP_TESTS_DIR' ) ) ? getenv( 'WP_TESTS_DIR' ) : $_root_dir . '/tmp/wordpress-tests-lib';
-$_tests_dir = $_root_dir . '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 require_once $_root_dir . '/vendor/autoload.php';
